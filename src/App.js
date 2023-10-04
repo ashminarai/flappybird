f4af595aedb065f92bfb58cf4f0553137922e7ae
ashminarai <raiashmina404@gmail.com>
2023-10-04 17:07:40 +0545

    developed game successfully

--- src/App.js
+++ src/App.js
@@ -1,145 +1,84 @@
-// import "./App.css";
-// import React, { useState, useEffect } from "react";
-// import styled from "styled-components";
-
-// const WALL_HEIGHT = 600;
-// const WALL_WIDTH = 400;
-
-// const BIRD_WIDTH = 33;
-// const BIRD_HEIGHT = 28;
-
-// const GRAVITY = 5;
-// function App() {
-//   const [birdposition, setBirdposition] = useState(200);
-
-//   useEffect(() => {
-//     let birdval;
-
-//     if (birdposition < WALL_HEIGHT - BIRD_HEIGHT) {
-//       birdval = setInterval(() => {
-//         setBirdposition((birdposition) => birdposition + GRAVITY);
-//       }, 24);
-//     }
-//     return () => clearInterval(birdval);
-//   });
-
-//   const handler = () => {
-//     if (birdposition < BIRD_HEIGHT) {
-//       setBirdposition(0);
-//     } else {
-//       setBirdposition((birdposition) => birdposition - 50);
-//     }
-//   };
-
-//   return (
-//     <Home onClick={handler}>
-//       <Startgame>Click to start</Startgame>
-//       <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
-//         <Bird
-//           height={BIRD_HEIGHT}
-//           width={BIRD_WIDTH}
-//           top={birdposition}
-//           left={100}
-//         />
-//       </Background>
-//     </Home>
-//   );
-// }
-
-// export default App;
-
-// const Home = styled.div`
-//   height: 100vh;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   position: relative;
-// `;
-
-// const Background = styled.div`
-//   background-image: url("/images/background-day.png"); // Adjust the path to your image
-//   background-repeat: no-repeat;
-//   background-size: ${(props) => props.width}px ${(props) => props.height}px;
-//   width: ${(props) => props.width}px;
-//   height: ${(props) => props.height}px;
-//   border: 2px solid black;
-//   position: relative;
-// `;
-
-// const Bird = styled.div`
-//   background-repeat: no-repeat;
-//   position: absolute;
-//   background-image: url("/images/yellowbird-upflap.png");
-//   background-size: ${(props) => props.width}px ${(props) => props.height}px;
-//   width: ${(props) => props.width}px;
-//   height: ${(props) => props.height}px;
-//   top: ${(props) => props.top}px;
-//   left: ${(props) => props.left}px;
-// `;
-
-// const Startgame = styled.div`
-//   text-align: center;
-//   position: relative;
-//   top: 49%;
-//   background-color: black;
-//   padding: 10px;
-//   width: 100px;
-//   left:50%;
-//   margin-left:-55px;
-//   color:#fff;
-//   font-size:20px;
-//   border-radius:10px;
-// `;
-
-import "./App.css";
-import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
+// Constants for bird, wall, and obstacle sizes and physics
+const BIRD_HEIGHT = 28;
+const BIRD_WIDTH = 33;
 const WALL_HEIGHT = 600;
 const WALL_WIDTH = 400;
-
-const BIRD_WIDTH = 33;
-const BIRD_HEIGHT = 28;
-
-const GRAVITY = 5;
+const GRAVITY = 8;
+const OBJ_WIDTH = 52;
+const OBJ_SPEED = 6;
+const OBJ_GAP = 200;
 
 function App() {
-  const [birdposition, setBirdposition] = useState(200);
+  // State variables for game control
+  const [isStart, setIsStart] = useState(false);
+  const [birdpos, setBirspos] = useState(300);
+  const [objHeight, setObjHeight] = useState(0);
+  const [objPos, setObjPos] = useState(WALL_WIDTH);
+  const [score, setScore] = useState(0);
 
-  const [isStart, setIsstart] = useState(false);
-
+  // Update bird's position based on gravity
   useEffect(() => {
-    let birdval;
-
-    if (isStart && birdposition < WALL_HEIGHT - BIRD_HEIGHT) {
-      birdval = setInterval(() => {
-        setBirdposition((birdposition) => birdposition + GRAVITY);
+    let intVal;
+    if (isStart && birdpos < WALL_HEIGHT - BIRD_HEIGHT) {
+      intVal = setInterval(() => {
+        setBirspos((birdpos) => birdpos + GRAVITY);
       }, 24);
     }
-    return () => clearInterval(birdval);
-  });
+    return () => clearInterval(intVal);
+  }, [isStart, birdpos]);
 
-  const handler = () => {
-    if (!isStart) setIsstart(true);
-    else if (birdposition < BIRD_HEIGHT) {
-      setBirdposition(0);
+  // Move the obstacle and update score
+  useEffect(() => {
+    let objval;
+    if (isStart && objPos >= -OBJ_WIDTH) {
+      objval = setInterval(() => {
+        setObjPos((objPos) => objPos - OBJ_SPEED);
+      }, 24);
+
+      return () => {
+        clearInterval(objval);
+      };
     } else {
-      setBirdposition((birdposition) => birdposition - 50);
+      setObjPos(WALL_WIDTH);
+      setObjHeight(Math.floor(Math.random() * (WALL_HEIGHT - OBJ_GAP)));
+      if (isStart) setScore((score) => score + 1);
     }
+  }, [isStart, objPos]);
+
+  // Check for collisions and reset game
+  useEffect(() => {
+    let topObj = birdpos >= 0 && birdpos < objHeight;
+    let bottomObj =
+      birdpos <= WALL_HEIGHT &&
+      birdpos >=
+        WALL_HEIGHT - (WALL_HEIGHT - OBJ_GAP - objHeight) - BIRD_HEIGHT;
+
+    if (
+      objPos >= OBJ_WIDTH &&
+      objPos <= OBJ_WIDTH + 80 &&
+      (topObj || bottomObj)
+    ) {
+      setIsStart(false);
+      setBirspos(300);
+      setScore(0);
+    }
+  }, [isStart, birdpos, objHeight, objPos]);
+
+  // Click handler to start game or flap bird
+  const handler = () => {
+    if (!isStart) setIsStart(true);
+    else if (birdpos < BIRD_HEIGHT) setBirspos(0);
+    else setBirspos((birdpos) => birdpos - 50);
   };
 
   return (
     <Home onClick={handler}>
-      {/* <span>Score: {score}</span> */}
       <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
-        {!isStart ? <Startgame>Click To Start</Startgame> : null}
-        <Bird
-          height={BIRD_HEIGHT}
-          width={BIRD_WIDTH}
-          top={birdposition}
-          left={100}
-        />
-        {/* <Obj
+        {!isStart ? <Startboard>Click To Start</Startboard> : null}
+        <Obj
           height={objHeight}
           width={OBJ_WIDTH}
           left={objPos}
@@ -158,7 +97,8 @@ function App() {
           left={objPos}
           top={WALL_HEIGHT - (objHeight + (WALL_HEIGHT - OBJ_GAP - objHeight))}
           deg={0}
-        /> */}
+        />
+        {isStart && <ScoreShow>Score: {score}</ScoreShow>}
       </Background>
     </Home>
   );
@@ -174,19 +114,21 @@ const Home = styled.div`
 `;
 
 const Background = styled.div`
-  background-image: url("/images/background-day.png"); // Adjust the path to your image
+  background-image: url("./images/background-day.png");
   background-repeat: no-repeat;
   background-size: ${(props) => props.width}px ${(props) => props.height}px;
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
-  border: 2px solid black;
   position: relative;
+  overflow: hidden;
+  border: 2px solid black;
+
 `;
 
 const Bird = styled.div`
-  background-repeat: no-repeat;
   position: absolute;
-  background-image: url("/images/yellowbird-upflap.png");
+  background-image: url("./images/yellowbird-upflap.png");
+  background-repeat: no-repeat;
   background-size: ${(props) => props.width}px ${(props) => props.height}px;
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
@@ -194,17 +136,41 @@ const Bird = styled.div`
   left: ${(props) => props.left}px;
 `;
 
-const Startgame = styled.div`
-  text-align: center;
-  position: absolute;
-  top: 50%; /* Center vertically */
-  left: 50%; /* Center horizontally */
-  transform: translate(-50%, -50%); /* Center the text within its parent */
+const Obj = styled.div`
+  position: relative;
+  background-image: url("./images/pipe-green.png");
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  left: ${(props) => props.left}px;
+  top: ${(props) => props.top}px;
+  transform: rotate(${(props) => props.deg}deg);
+`;
+
+const Startboard = styled.div`
+  position: relative;
+  top: 49%;
   background-color: black;
   padding: 10px;
   width: 100px;
-  color: #fff;
+  left: 50%;
+  margin-left: -50px;
+  text-align: center;
   font-size: 20px;
   border-radius: 10px;
-  cursor: pointer;
+  color: #fff;
+  font-weight: 600;
+`;
+
+const ScoreShow = styled.div`
+  text-align: center;
+  background-color: black;
+  padding: 10px;
+  width: 100px;
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  color: #fff;
+  font-size: 18px;
+  border-radius: 10px;
 `;
