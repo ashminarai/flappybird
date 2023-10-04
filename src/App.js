8f4f6a5ca29afa17ece47c86bec6d0b112158e1f
ashminarai <raiashmina404@gmail.com>
2023-10-04 15:52:36 +0545

    played with the upflap pictures

--- src/App.js
+++ src/App.js
@@ -44,11 +44,14 @@ import styled from "styled-components";
 const WALL_HEIGHT = 600;
 const WALL_WIDTH = 400;
 
+const BIRD_WIDTH = 33;
+const BIRD_HEIGHT = 28;
+
 function App() {
   return (
     <Home>
       <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
-        <Bird />
+        <Bird height={BIRD_HEIGHT} width={BIRD_WIDTH} top={300}/>
       </Background>
     </Home>
   );
@@ -66,11 +69,20 @@ const Home = styled.div`
 const Background = styled.div`
   background-image: url("/images/background-day.png"); // Adjust the path to your image
   background-repeat: no-repeat;
-  background-size: ${props => props.width}px ${props => props.height}px;
-  width: ${props => props.width}px;
-  height: ${props => props.height}px;
+  background-size: ${(props) => props.width}px ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
   border: 2px solid black;
+  position:relative;
 `;
 
-const Bird = styled.div``;
-
+const Bird = styled.div`
+  background-repeat: no-repeat;
+  position: absolute;
+  background-image: url("/images/yellowbird-upflap.png");
+  background-size: ${(props) => props.width}px ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  top:${(props) => props.top}px;
+  left: 100px;
+`;
